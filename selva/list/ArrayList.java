e4a40b58ac1e519421cc306404753bc1d5490ff4
selvaganeshoo <selvarrs0912@gmail.com>
2025-09-26 15:41:28 +0530

    Friday

--- selva/list/ArrayList.java
+++ selva/list/ArrayList.java
@@ -5,8 +5,19 @@ public class ArrayList{
 	private Object[] al;
 	public ArrayList(){
 		this.index = -1;
-		this.capacity = 10;
+		this.capacity = 0;
 		this.al = new Object[capacity];
+	}
+	public ArrayList(int capacity){
+		this. capacity = capacity;
+		this.index = -1;
+		this.al = new Object[capacity];
+	}
+	public int capacity(){
+		return this.capacity;
+	}
+	public int size(){
+		return this.index+1;
 	}
 	@Override
 	public String toString(){
@@ -20,17 +31,21 @@ public class ArrayList{
 		sb.append("]");
 		return sb.toString();
 	}
+	private String outOfBound(int ind){
+		return "Index: "+ ind + " size: " + index;
+	}
 	private void grow(){
 		this.capacity+=10;
 		al = Arrays.copyOf(al,capacity);
 	}
-	private void swifter(int ind,Object o){
-		Object s1 = null;
-		Object s1 = o;
-		for(int i=ind;i<index+2;i++){
-			s1 = al[i];
-			al[i] = s2;
-			s2 = s1;
+	private void swifter(int ind){
+		for(int i = index;i>=ind;i--){
+			al[i+1] = al[i];
+		}
+	}
+	private void swapper(int ind){
+		for(int i=ind;i<index;i++){
+			al[i] = al[i+1];
 		}
 	}
 	public boolean add(Object o){
@@ -40,14 +55,81 @@ public class ArrayList{
 	al[++index] = o;
 	return true;
 	}
+	public Object remove(int ind){
+		Object var1 = al[ind];
+		swapper(ind);
+		--index;
+		return var1;
+	}
+	public boolean remove(Object o){
+		for(int i=0;i<=index;i++){
+			if(al[i] == o){
+				swapper(i);
+				--index;
+				return true;
+			}
+		}
+		return false;
+	}
 	public void add(int ind,Object o){
-		if(index+1 < capacity){
-			swifter(int ind,Object o);
-		}
-		}else if(index+1 == capacity){
+		if(index+1 == capacity){
 			grow();
-			swifter();
+			swifter(ind);
+			++index;
+			al[ind] = o;
 		}
-	++index;
+		else if(ind >= index+2||ind<0){
+			throw new ArrayIndexOutOfBoundsException(this.outOfBound(ind));
+		}else if(index+1 < capacity) {
+			swifter(ind);
+			++index;
+			al[ind] = o;
+		}
+	}
+	public Object set(int ind,Object o){
+		if(ind>index||ind<0){
+			throw new ArrayIndexOutOfBoundsException(this.outOfBound(ind));
+		}
+		al[ind] = o;
+		return o;
+	}
+	public void clear(){
+		for(int i=0;i<=index;i++){
+			al[i] = null;
+		}
+		index = -1;
+	}
+	public boolean isEmpty(){
+		return this.index==-1;
+	}
+	public Object get(int ind){
+		if(ind>index||ind<0){
+			throw new ArrayIndexOutOfBoundsException(this.outOfBound(ind));
+		}
+		return al[ind];
+	}
+	public int indexOf(Object o){
+		for(int i=0;i<=index;i++){
+			if(al[i] == o){
+				return i;
+			}
+		}
+		return -1;
+	}
+	public int lastIndexOf(Object o){
+		for(int i=index;i>=0;i--){
+			if(al[i] == o){
+				return i;
+			}
+		}
+		return -1;
+	}
+	public boolean contains(Object o){
+		return indexOf(o)>=0;
+	}
+	public void trimSize(){
+		this.capacity = index+1;
+		al = Arrays.copyOf(al,capacity);
 	}
 }
+
--- selva/list/CheckA.java
+++ selva/list/CheckA.java
@@ -1,10 +1,25 @@
 public class CheckA{
 	public static void main(String[] args){
 		ArrayList a = new ArrayList();
+		a.add("hello");
+		a.add("hi");
+		a.add("Object");
+		a.add("how");
+		a.add("are");
+		a.add("you");
 		a.add(1);
 		a.add(2);
+		a.add(3);
 		a.add(4);
-		a.add(2,3);
+		System.out.println(a.contains("you"));
+		System.out.println(a.capacity());
+		a.add("new");
+		a.add("Object1");
+		a.add(24);
+		System.out.println(a.capacity());
+		a.trimSize();
+		System.out.println("capacity: "+a.capacity());
 		System.out.println(a);
+		System.out.println("index: "+a.size());
 	}
 }
